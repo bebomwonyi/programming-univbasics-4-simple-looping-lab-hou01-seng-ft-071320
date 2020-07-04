b2a9e02a48e3a7b35bdd214b7989c45bec4f4230
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-04 12:12:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,25 @@
 # Write your methods here
+def loop_message_five_times(thing)
+  5.times{puts thing}
+end
+
+def loop_message_n_times(thing , n)
+  n.times{puts thing}
+end
+
+def output_array(n)
+  count = 0
+  while n[count] do
+    puts n[count]
+    count += 1
+  end
+end
+
+def return_string_array(n)
+  narray = []
+  
+  while n.length > 0 do
+    narray << "#{n[n.length]}"
+  end
+  return narray
+end
